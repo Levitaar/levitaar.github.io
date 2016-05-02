825a5bc63a54b01a225b8e70791ef3432d2e3d91
Luis Gerardo Huiza Blanco <LGHUIZA@Luiss-MBP.wireless.newschool.edu>
2016-05-02 13:30:30 -0400

    HW UPDATE 2

--- 02/class2.js
+++ 02/class2.js
@@ -2,10 +2,10 @@ function madlib() {
 
     adj1 = document.getElementById("adj1");
     noun1 = document.getElementById("noun1");
+    noun2 = document.getElementById("noun2");
 
     var para = document.createElement("P");
-    var t = document.createTextNode("this is a " + adj1.value + " " + noun1.value ", if feels like"
-    );
+    var t = document.createTextNode("this is a " + adj1.value + " " + noun1.value + ", it feels like " + noun2.value);
     para.appendChild(t);
     document.getElementById("myDIV").appendChild(para);
 }
